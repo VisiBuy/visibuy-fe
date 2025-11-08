c55e449b5d6ec412b307e9bb479b452d59cbe876
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2025-11-08 15:24:27 +0100

    Created seller profile folder

---
+++ src/pages/SellerProfile/SellerProfile.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const SellerProfile = () => {
+    return (
+        <main>
+            <header>
+                <nav>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+        </main>
+    )
+}
