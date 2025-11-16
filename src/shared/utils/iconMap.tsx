dae5b062a65e65c9a6bfa5e330cfcf96571c9943
Gogo602 <gogomarosi61@gmail.com>
2025-11-16 18:21:31 +0300

    Notification icon

--- src/shared/utils/iconMap.tsx
+++ src/shared/utils/iconMap.tsx
@@ -11,6 +11,7 @@ import {
   FiX,
   FiLogOut,
   FiChevronRight,
+  FiBell 
 } from "react-icons/fi";
 
 /**
@@ -32,6 +33,7 @@ export const iconMap: Record<
   close: FiX,
   logout: FiLogOut,
   chevronRight: FiChevronRight,
+  notification: FiBell,
 };
 
 /**
