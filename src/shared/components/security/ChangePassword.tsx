852510121ffb2ab5c73af0397424dd59d42c0ab1
Babs-unique <145204996+Babs-unique@users.noreply.github.com>
2025-12-26 07:32:19 +0100

    QA on the seller profile and security page

--- src/shared/components/security/ChangePassword.tsx
+++ src/shared/components/security/ChangePassword.tsx
@@ -1,14 +1,35 @@
 import { useChangePasswordMutation } from '@/features/security/passwordApi';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import eyeOn from '../../.././assets/icons/eye.svg'
+import eyeOff from '../../.././assets/icons/eye-off.svg'
 
 const ChangePassword: React.FC = () => {
   const [currentPassword, setCurrentPassword] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [showCurrent, setShowCurrent] = useState(false);
+  const [showNew, setShowNew] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [windowWidth , setWindowWidth] = useState(window.innerWidth)
+
+
 
   const [error, setError] = useState<string | null>(null);
   const [success, setSuccess] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
+
+
+  useEffect(() => {
+    const handleResize = () => setWindowWidth(window.innerWidth);
+  
+    window.addEventListener("resize", handleResize);
+  
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const currentPasswordPlaceholder = windowWidth < 425 ? "Current Password" : "Enter Current Password"
+  const newPasswordPlaceholder = windowWidth < 425 ? "New Password" : "Enter New Password"
+  const confirmNewPassword = windowWidth < 425 ? "Confirm Password" : "Re-enter New Password"
 
   // RTK Query mutation hook
   const [changePassword, { isLoading }] = useChangePasswordMutation();
@@ -24,6 +45,11 @@ const ChangePassword: React.FC = () => {
   };
 
   const passwordStrengthError = validatePasswordStrength(newPassword);
+
+  const toggleCurrent = () => setShowCurrent(prev => !prev);
+  const toggleNew = () => setShowNew(prev => !prev);
+  const toggleConfirm = () => setShowConfirm(prev => !prev);
+  
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -73,40 +99,46 @@ const ChangePassword: React.FC = () => {
   };
 
   return (
-    <div className="bg-white rounded-xl border border-gray-200 p-6 mb-6">
+    <div className="bg-white rounded-xl border border-gray-200 p-6 mb-6 w-full">
       <h2 className="text-xl font-bold text-gray-900 mb-6">Change Password</h2>
       
       <form onSubmit={handleSubmit} className="space-y-4">
         {/* Current Password */}
-        <div>
+        <div className='relative' >
           <label htmlFor="current-password" className="block text-sm font-medium text-gray-700 mb-2">
             Current Password
           </label>
           <input
             id="current-password"
-            type="password"
+            type={showCurrent ? "text" : "password"}
             value={currentPassword}
             onChange={(e) => setCurrentPassword(e.target.value)}
-            className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent transition-smooth"
-            placeholder="Enter current password"
+            className="w-full px-4 py-3 border  border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent transition-smooth"
+            placeholder={currentPasswordPlaceholder}
             required
           />
+          <img src={showCurrent ? eyeOn : eyeOff } alt="toggle-shown" 
+          onClick={toggleCurrent}
+          className='absolute right-3 top-10' />
         </div>
 
         {/* New Password */}
-        <div>
+        <div className='relative'>
           <label htmlFor="new-password" className="block text-sm font-medium text-gray-700 mb-2">
             New Password
           </label>
           <input
             id="new-password"
-            type="password"
+            type={showNew ? "text" : "password"}
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
             className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent transition-smooth"
-            placeholder="Enter new password"
+            placeholder={newPasswordPlaceholder}
             
           />
+          <img src={showNew ? eyeOn : eyeOff } alt="toggle-shown" 
+          onClick={toggleNew}
+          className='absolute right-3 top-10' />
           <p className="mt-2 text-xs text-gray-500">
             Must be at least 8 characters with uppercase, lowercase, and numbers
           </p>
@@ -127,18 +159,21 @@ const ChangePassword: React.FC = () => {
       )}
 
         {/* Confirm New Password */}
-        <div>
+        <div className='relative'>
           <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700 mb-2">
             Confirm New Password
           </label>
           <input
             id="confirm-password"
-            type="password"
+            type={showConfirm ? "text" : "password"}
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-primary focus:border-transparent transition-smooth"
-            placeholder="Re-enter new password"
+            placeholder={confirmNewPassword}
           />
+          <img src={showConfirm ? eyeOn : eyeOff } alt="toggle-shown" 
+          onClick={toggleConfirm}
+          className='absolute right-3 top-10' />
         </div>
 
         {error && <p style={{ color: "red" }}>{error}</p>}
